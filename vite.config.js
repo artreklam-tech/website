aab10bfdefc73cfae8e8c82c10cb22838ce62e9d
Ari Younis <info.solomove@gmail.com>
2025-11-30 19:17:26 +0000

    should be good to publish

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set base for GitHub Pages to ensure assets resolve correctly
+  base: '/website/',
 })
